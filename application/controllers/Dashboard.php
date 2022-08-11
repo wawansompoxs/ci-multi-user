aa61fd8ed719d0136d9065501a729865ce744ad0
wawansompoxs <wawansompoxs@yahoo.co.id>
2022-08-11 11:51:49 +0700

    Penambahan hitunganstatistik

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -9,16 +9,22 @@ class Dashboard extends MY_Controller {
         $this->load->library('fungsi');
         $this->load->library('user_agent');
         $this->load->helper('myfunction_helper');
+		$this->load->model(array('Mod_dashboard'));
         // backButtonHandle();
     }
 
     function index()
     {
+		$data['count_all'] = $this->Mod_dashboard->count_all();
+		$data['count_pns'] = $this->Mod_dashboard->count_pns();
+		$data['count_cpns'] = $this->Mod_dashboard->count_cpns();
+		$data['count_pppk'] = $this->Mod_dashboard->count_pppk();
+		$data['count_honor'] = $this->Mod_dashboard->count_honor();
     	$logged_in = $this->session->userdata('logged_in');
         if ($logged_in != TRUE || empty($logged_in)) {
             redirect('login');
         }else{
-        	$this->template->load('layoutbackend','dashboard/dashboard_data');
+        	$this->template->load('layoutbackend','dashboard/dashboard_data',$data);
         }
         
     }
