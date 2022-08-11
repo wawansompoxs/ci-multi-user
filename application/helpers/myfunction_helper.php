c13591fbe74ae0d424a8ff2637d0ef1f5468eb33
wawansompoxs <wawansompoxs@yahoo.co.id>
2022-08-11 16:19:48 +0700

    benerin array bulan

--- application/helpers/myfunction_helper.php
+++ application/helpers/myfunction_helper.php
@@ -6,8 +6,7 @@ function tgl_indonesia($date)
 	/* array hari dan bulan */
 	$nama_hari  = array("Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu");
 
-	$nama_bulan = array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober",
-	                    "November","Desember");
+	$nama_bulan = array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember");
 
 	/*  Memisahkan format tanggal, bulan, tahun dengan substring */
 	$tahun   = substr($date, 0, 4);
